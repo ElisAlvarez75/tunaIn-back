70611c0f7e0fee55bf13236f96fb5923ce53393e
SalvaSC <sanchezcampossalvador@gmail.com>
2021-01-18 13:05:34 +0100

    arreglado index.js en carpeta mongo

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -1,7 +1,6 @@
 require('./connection');
 const User = require('./schemas/User');
 const Podcast = require('./schemas/Podcast');
-const Comment = require("./schemas/Comment");
 const Favoritos = require('./schemas/Favoritos');
 const Follows = require('./schemas/Follows');
 const Comment = require("./schemas/Comment");
