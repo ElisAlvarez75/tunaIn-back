1eb4caab015bd1182f07e538ecfe74c41daed78e
Elis <elisalvarez@msn.com>
2021-01-22 20:12:11 +0100

    Deployment segundo

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const jwtSecret = process.env.JWT_SECRET || 'meow';
 const adminPassword = process.env.ADMIN_PASSWORD || 'meow';
 const adminEmail = process.env.ADMIN_EMAIL || 'admin@admin.com';
 
+
 module.exports = {
   jwtSecret,
   adminPassword,
